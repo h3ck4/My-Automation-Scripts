06ccfd2a0b44e47179ef9e189fdd971298683acf
h3ck4 <128188156+h3ck4@users.noreply.github.com>
2025-11-30 16:37:05 +0000

    Update and rename change_system_audio_output.sh to change_system_audio_output_with_one_key.sh
    changing this file name

--- change_system_audio_output_with_one_key.sh
+++ change_system_audio_output_with_one_key.sh
@@ -5,7 +5,7 @@ HEADPHONE="alsa_output.usb-Elite_Silicon_USB_Audio_Device-00.analog-stereo"
 SYSTEM="alsa_output.pci-0000_0f_00.6.analog-stereo"
 
 # Get current default sink
-CURRENT_SINK=$(pactl info | grep "Default Sink" | awk '{print $3}')
+CURRENT_SINK=$(pactl info | grep "Default Sink" | awk '{Print $3}')
 
 # Decide new sink (toggle)
 if [[ "$CURRENT_SINK" == "$HEADPHONE" ]]; then
@@ -20,9 +20,9 @@ echo "Switching audio from $CURRENT_SINK to $NEW_SINK"
 pactl set-default-sink "$NEW_SINK"
 
 # Move all active streams to new sink
-for INPUT in $(pactl list short sink-inputs | awk '{print $1}'); do
+for INPUT in $(pactl list short sink-inputs | awk '{Print $1}'); do
   pactl move-sink-input "$INPUT" "$NEW_SINK"
-done
+Done
 
 
 # this file will make your linux mint or any other linux to switch between different audio input with one ( 1 ) key. so when you hit for instance fX or any keyX, you can swap your audio ouput to headphone for instance... its cool you need to change it though.
